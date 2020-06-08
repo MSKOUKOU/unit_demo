687e539dd838553b4cb56b8fb083bbc489bcc1c3
koujin <1306629190@qq.com>
2020-06-08 11:43:09 +0800

    测试提交

---
+++ src/views/demo-page-one/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>测试一下</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: [""],
+  data() {
+    return {};
+  },
+  /* 组件 */
+  components: {},
+  /* HTML DOM加载后马上执行的，如赋值*/
+  computed: {},
+  /* 模板渲染成html前调用，即通常初始化某些属性值，然后再渲染成视图 */
+  created() {},
+  /* 模板渲染成html后调用，通常是初始化页面完成后，再对html的dom节点进行一些需要的操作 */
+  mounted() {},
+  /* methods则必须要有一定的触发条件才能执行 */
+  methods: {},
+  /* 观察Vue实例上的数据变动 */
+  watch: {}
+};
+</script>
+<style lang='' scoped>
+</style>
